ad8e272a6c02b91562e42ce0e5f48243be392c48
Ghaith A <alghaith.ah38@gmail.com>
2024-06-26 20:27:15 -0400

    Added another endpoint for /info. Step 2

--- index.js
+++ index.js
@@ -30,6 +30,16 @@ app.get('/api/persons', (request, response) => {
     response.send(persons)
 })
 
+app.get('/info', (request, response) => {    
+    const currentDate = new Date()
+    response.send(
+        `
+        <p>Phonebook has info for ${persons.length} people</p>
+        <p>${currentDate}</p>
+        `
+    )
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`)
