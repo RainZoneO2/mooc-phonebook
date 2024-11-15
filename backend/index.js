e548643d673043dfc736e44af99a1281e0b12bdf
Ghaith A <alghaith.ah38@gmail.com>
2024-11-15 01:37:05 -0500

    Updated to allow for deletion to be reflected in database. Removed redundent persons list. Step 15.

--- backend/index.js
+++ backend/index.js
@@ -14,42 +14,8 @@ morgan.token('body', function (req) {
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time :body'))
 
-let persons =
-[
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "14-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
 const cors = require('cors')
 app.use(cors())
-
-const generateId = () => {
-    const id = Math.floor(Math.random() * 10000)
-
-    if (persons.find(person => Number(person.id) === id)) {
-        return generateId()
-    }
-
-    return String(id)
-}
 
 app.get('/api/persons', (request, response) => {
     Person.find({}).then(persons => {
@@ -80,10 +46,12 @@ app.get('/info', (request, response) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    persons = persons.filter(person => person.id !== id)
+    Person.findByIdAndDelete(request.params.id).then(result => {
+        response.status(204).end()
+    }).catch(error => {
+        console.log(`Errpr deleting ${request.params.id}: ${error}`)
+    })
 
-    response.status(204).end()
 })
 
 app.post('/api/persons', (request, response) => {
